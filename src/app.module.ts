0491373141f2328a802911c31a8073f9936c1a91
Vincent Monjaret <vmonjaret@gmail.com>
2022-09-12 12:40:07 +0200

    chore: add pino logger

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { APP_INTERCEPTOR } from '@nestjs/core';
+import { LoggerModule } from 'nestjs-pino';
+
 import { ApmInterceptor } from './apm.interceptor';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [LoggerModule.forRoot()],
   controllers: [AppController],
   providers: [
     AppService,
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import * as apm from 'elastic-apm-node';
+import { Logger } from 'nestjs-pino';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -8,7 +9,9 @@ async function bootstrap() {
     environment: 'local',
   });
 
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { bufferLogs: true });
+  app.useLogger(app.get(Logger));
+
   await app.listen(3000);
 }
 bootstrap();
