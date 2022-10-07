87a3f2e05f83a20068d87b2153240a4a8939b527
Vincent Monjaret <vmonjaret@gmail.com>
2022-10-07 09:22:10 +0200

    chore(swagger): added swagger documentation

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { Logger } from 'nestjs-pino';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { bufferLogs: true });
   app.useLogger(app.get(Logger));
+
+  const config = new DocumentBuilder()
+    .setTitle('Bazinga')
+    .setDescription('Semantic release sandbox')
+    .setVersion(process.env.npm_package_version)
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(3000);
 }
