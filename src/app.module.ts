334b14969280d0481f096b6db14d799891378266
Vincent Monjaret <vmonjaret@gmail.com>
2022-10-07 10:34:09 +0200

    feat(greeting)!: removing greeting endpoint

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,12 @@
 import { Module } from '@nestjs/common';
 import { APP_INTERCEPTOR } from '@nestjs/core';
 import { LoggerErrorInterceptor, LoggerModule } from 'nestjs-pino';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { PostsModule } from './posts/posts.module';
 
 @Module({
   imports: [PostsModule, LoggerModule.forRoot()],
-  controllers: [AppController],
+  controllers: [],
   providers: [
-    AppService,
     {
       provide: APP_INTERCEPTOR,
       useClass: LoggerErrorInterceptor,
